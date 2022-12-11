5afb8c10a3ea8a5c198aeddd2b28b0cb0feb5575
Tailer Nguyen <tailerchimchar@gmail.com>
2022-12-11 02:01:34 -0600

    adding all code first to main branch

--- moneyTracker/MoneyGoByeBye/MoneyGoByeBye/Form1.Designer.cs
+++ moneyTracker/MoneyGoByeBye/MoneyGoByeBye/Form1.Designer.cs
@@ -1,4 +1,20 @@
-﻿namespace MoneyGoByeBye
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Syncfusion.Maui.Charts;
+
+namespace MoneyGoByeBye
 {
     partial class Form1
     {
@@ -44,6 +60,8 @@
             this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -59,7 +77,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(33, 119);
+            this.label2.Location = new System.Drawing.Point(23, 119);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 25);
             this.label2.TabIndex = 1;
@@ -68,7 +86,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(33, 222);
+            this.label4.Location = new System.Drawing.Point(23, 222);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(100, 25);
             this.label4.TabIndex = 3;
@@ -76,7 +94,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(33, 250);
+            this.textBox1.Location = new System.Drawing.Point(23, 250);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(186, 31);
             this.textBox1.TabIndex = 4;
@@ -84,7 +102,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(33, 314);
+            this.label5.Location = new System.Drawing.Point(23, 314);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(84, 25);
             this.label5.TabIndex = 5;
@@ -93,7 +111,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(33, 412);
+            this.label6.Location = new System.Drawing.Point(23, 412);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(186, 25);
             this.label6.TabIndex = 6;
@@ -101,21 +119,21 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(33, 342);
+            this.textBox2.Location = new System.Drawing.Point(23, 342);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(186, 31);
             this.textBox2.TabIndex = 7;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(33, 440);
+            this.textBox3.Location = new System.Drawing.Point(23, 440);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(186, 31);
             this.textBox3.TabIndex = 8;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(33, 511);
+            this.button1.Location = new System.Drawing.Point(23, 509);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(197, 34);
             this.button1.TabIndex = 9;
@@ -169,7 +187,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(33, 190);
+            this.checkBox1.Location = new System.Drawing.Point(23, 184);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(175, 29);
             this.checkBox1.TabIndex = 11;
@@ -183,11 +201,31 @@
             this.dateTimePicker1.Size = new System.Drawing.Size(300, 31);
             this.dateTimePicker1.TabIndex = 12;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 870);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(243, 34);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Show As Pie Chart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Location = new System.Drawing.Point(23, 923);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(500, 500);
+            this.panel2.TabIndex = 14;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(878, 957);
+            this.ClientSize = new System.Drawing.Size(924, 1385);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.dataGridView1);
@@ -227,5 +265,7 @@
         private DataGridViewTextBoxColumn Description;
         private CheckBox checkBox1;
         private DateTimePicker dateTimePicker1;
+        private Button button2;
+        private Panel panel2;
     }
 }
